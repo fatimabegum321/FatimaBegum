496dfedf0aa93e109e338d6052c6ccbd88599a92
fatimabegum321 <31396318+fatimabegum321@users.noreply.github.com>
2017-08-27 23:58:23 -0500

    Create  MainFileIndex.php

---
+++ FileIndex/MainFileIndex.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<span>Hint: Please enter input as test1.php or test2.php or test3.php </span><br>
+
+There are 3 books test files given if you will not enter any test.php n upload it will display results from test1.php by default
+<form enctype="multipart/form-data" action="FileIndex2.php" method="post">
+    <input name="fileName" id="fileName" type="text"/>
+    <input type="submit" name="submit" value="Upload" /><br/>
+</form>
+</body>
+</html>
